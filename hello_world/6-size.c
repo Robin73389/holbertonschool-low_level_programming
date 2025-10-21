c97d5ab6267a008b938c2deb8745ffe51daa1d51
Robin73389 <12229@holbertonstudents.com>
2025-10-21 13:52:49 +0200

    Change the script

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,15 +1,16 @@
-#include <stdio.h>                                                                                                                  		            
+// C Program to Find the Size of int, float, double, and
+// char using sizeof operator directly
+#include <stdio.h>
 
-//*
-* main - Entry point
-* Return: Always 0 (Success)
-*/
-int main (void) 
-{	
-printf("Size of a char: %lu byte(s)\n", sizeof(char));
-printf("Size of an int: %lu byte(s)\n", sizeof(int));
-printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-printf("Size of a float: %lu byte(s)\n", sizeof(float));
-return (0);
-}
+int main() {
+
+	printf("Size of int: %u bytes\n", sizeof(int));
+	 
+	printf("Size of float: %u bytes\n", sizeof(float));
+
+	printf("Size of double: %u bytes\n", sizeof(double));
+	
+	printf("Size of char: %u bytes\n", sizeof(char));
+
+	return 0;
+}	
