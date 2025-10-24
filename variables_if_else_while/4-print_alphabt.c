36b23bf459543ec352da8bebb72bd08e62966c8b
Robin73389 <12229@holbertonstudents.com>
2025-10-24 15:34:41 +0200

    Add the description

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
-int main(){
-    char ch;
 
-    for (ch = 'a'; ch <= 'z'; ch++){
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{	
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++){
     		
 	    if (ch != 'q' && ch != 'e')
 			putchar (ch);
