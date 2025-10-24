20864e5b86817e2643eb491b66eae6a779e363a6
Robin73389 <12229@holbertonstudents.com>
2025-10-24 15:36:19 +0200

    Add the description

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -10,11 +10,11 @@ int main(void)
 {	
 	char ch;
 
-	for (ch = 'a'; ch <= 'z'; ch++){
-    		
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{	
 	    if (ch != 'q' && ch != 'e')
 			putchar (ch);
     }
-    putchar('n');
+    putchar('\n');
     return 0;
 }
