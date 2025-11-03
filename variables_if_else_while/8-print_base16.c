061d1c77d4349917828c68add6eefe974cfa8863
Robin73389 <12229@holbertonstudents.com>
2025-11-03 11:03:57 +0100

    Add betty

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -5,17 +5,20 @@
 */
 int main(void)
 {
-    int numbers;
-    int ch;
-    for (numbers = '0'; numbers <= '9'; numbers++)
-    {
-	putchar(numbers);
-    }
-    {
-    for (ch = 'a' ; ch <='f'; ch++)
-	    putchar(ch);
-    }
+	int numbers;
+	int ch;
 
-    putchar('\n');
-    return (0);
+	for (numbers = '0'; numbers <= '9'; numbers++)
+	{
+		putchar(numbers);
+	}
+
+	for (ch = 'a' ; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
 }
