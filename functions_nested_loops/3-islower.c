b7f30c8b954d1e0a6fd630ba16a1f37884769d26
Robin73389 <12229@holbertonstudents.com>
2025-11-03 11:25:38 +0100

    Add the betty

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,10 +1,17 @@
 #include"main.h"
-
+/**
+ * write a function that checks for lowercase character
+ *
+ * Return: Always 0.
+ */
 int _islower(int c)
 {
-	if (c >= 97 && c <= 122) {
-	return 1;
-	} else {
-	return 0;
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
 	}
+	else
+
+		return (0);
+
 }
