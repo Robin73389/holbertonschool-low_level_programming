c9ea19f898558c0136be09ed56ff0b12ff7b7893
Robin73389 <12229@holbertonstudents.com>
2025-11-03 11:32:32 +0100

    Add the betty

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,5 +1,9 @@
 #include"main.h"
-
+/**
+ * _isalpha - the standard library provides
+ * @c: write a function that checks
+ * Return: always (0)
+ */
 int _isalpha(int c)
 {	
 	if ((c >='a' && c <='z') || (c >='A'&& c <='Z')) 
