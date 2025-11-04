d92054dc0f052d2bddc40ebfa0168f631fdc7615
Robin73389 <12229@holbertonstudents.com>
2025-11-04 01:44:09 +0100

    Write a function that prints n elements of an array of integers, followed by a new line

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,7 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-
+void reverse_array(int *a, int n);
+int _strcmp(char *s1, char *s2);
+char *_strncpy(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -14,4 +16,5 @@ void times_table(void);
 int add(int, int);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+void print_array(int *a, int n);
 #endif
