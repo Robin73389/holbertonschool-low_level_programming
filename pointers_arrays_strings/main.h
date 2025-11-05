959ac6464397c67dffc062ee365d1fbe4b7fa8d9
Robin73389 <12229@holbertonstudents.com>
2025-11-05 20:27:30 +0100

    Add the prototype char *_memcpy(char *dest, char *src, unsigned int n);

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -20,4 +20,5 @@ void print_array(int *a, int n);
 char *string_toupper(char *);
 char *_strchr(char *s, char c);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 #endif
