269579765118d2dba9df660f93b8bef5c827e827
Robin73389 <12229@holbertonstudents.com>
2025-11-12 22:05:22 +0100

    Add the function void init_dog(struct dog *d, char *name, float age, char *owner);

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,6 @@
-#ifndef DOG_H
+#ifndef DOG_H 
 #define DOG_H
+
 /**
  * struct dog - the name for the function
  * @name: the variable
@@ -12,4 +13,6 @@ struct dog
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #endif
