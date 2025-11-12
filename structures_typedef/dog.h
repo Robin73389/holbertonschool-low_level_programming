f34316776d29677b855fa381436ab5d21c44a3c4
Robin73389 <12229@holbertonstudents.com>
2025-11-12 21:11:13 +0100

    add the commente

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
-struct dog{	
+/**
+ * struct dog - the name for the function
+ * @name: the variable
+ * @age: the variable age
+ * @owner: the variable *owner poppy
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
