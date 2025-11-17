0ec72bc5d432589c382e8bec1e403c2caaf77e2e
Robin73389 <12229@holbertonstudents.com>
2025-11-17 21:07:13 +0100

    Prototype: char *_strcpy(char *dest, char *src);

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,15 +1,25 @@
+#include <stdio.h>
 #include "main.h"
-
+/**
+ * _strcpy - the name for the function
+ * @dest: the variable where are im going past the contenu for src
+ * @src: this is the string main
+ * Return: dest
+ */
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	while( src[i] != '\0')
-	{	
-		src[i] = dest[i];
+	if (dest == NULL || src == NULL)
+	{
+		return (0);
+	}
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
 		i++;
 	}
-	dest[i] = '\0';
 
-	return dest;
+	return (dest);
 }
