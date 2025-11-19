7df120321e827c097076d537186fb63d53045804
Robin73389 <12229@holbertonstudents.com>
2025-11-19 22:16:12 +0100

    Write a program that prints all arguments it receives.

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
-
+/**
+ * main - the prototype main
+ * @argc: serve to stocked the numbers d'argumente
+ * @argv: This is arrays of the pointer
+ * Return: (0);
+ */
 int main(int argc, char *argv[])
 {
-    int i;
-
-    for (i = 0; i < argc; i++)
-    {
-        printf("Arg %d : %s\n", i, argv[i]);
-    }
-
-    return 0;
+        if (argc > 1)
+        {
+                printf("%s\n", argv[0]);
+                printf("%s\n", argv[1]);
+                printf("%s\n", argv[2]);
+                printf("%s\n", argv[3]);
+                printf("%s\n", argv[4]);
+                printf("%s\n", argv[5]);
+                printf("%s\n", argv[6]);
+                printf("%s\n", argv[7]);
+        }
+        return (0);
 }
