df3a331e6886337b7f0dd709f8b94089f29d23f5
Robin73389 <12229@holbertonstudents.com>
2025-11-19 10:04:42 +0100

    Delete the condition the pointerls

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-#include <stddef.h>
+
 /**
  * print_name - my name function
  * @name: my pointer
@@ -7,7 +7,5 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
 	f(name);
 }
