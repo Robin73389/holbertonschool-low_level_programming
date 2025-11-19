4913902d9495bd7bf59ab4280613086d8d5ea531
Robin73389 <12229@holbertonstudents.com>
2025-11-19 22:52:56 +0100

    Correction the error message of segmentation

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -6,18 +6,11 @@
  * Return: (0);
  */
 int main(int argc, char *argv[])
-{
-	if (argc > 1)
+{	
+	int i;
+	for (i = 0; i < argc; i++)
 	{
-		printf("%s\n", argv[0]);
-		printf("%s\n", argv[1]);
-		printf("%s\n", argv[2]);
-		printf("%s\n", argv[3]);
-		printf("%s\n", argv[4]);
-		printf("%s\n", argv[5]);
-		printf("%s\n", argv[6]);
-		printf("%s\n", argv[7]);
-		printf("%s\n", argv[8]);
+		printf("%s\n", argv[i]);
 	}
 	return (0);
 }
