b0c9a125f87835681b61809bd690f913697cd2e9
Robin73389 <12229@holbertonstudents.com>
2025-11-19 10:37:13 +0100

    Add the comment for betty

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,15 +1,22 @@
 #include "function_pointers.h"
-
+#include <stddef.h>
+/**
+ * array_iterator - the name fort function
+ * @array: the pointeur
+ * @size: the size is the size of array
+ * @action: the pointer
+ * Return: 0;
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
-{	
+{
 	size_t i = 0;
-		
-	if( array == NULL || action == NULL)
-	
+
+	if (array == NULL || action == NULL)
+
 		return;
 
-	while ( i < size )
-	{	
+	while (i < size)
+	{
 		action(array[i]);
 		i++;
 	}
