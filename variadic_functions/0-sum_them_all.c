2a0ca6bc3149611d734f84f6945000d81960546e
Robin73389 <12229@holbertonstudents.com>
2025-11-25 21:17:44 +0100

    Add the condition betty

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,16 +1,21 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
+/**
+ * sum_them_all - This is my function on the sum parameters
+ * @n: this is my parameter
+ * Return: somme
+ */
 
 int sum_them_all(const unsigned int n, ...)
 {
 va_list ap;
- unsigned int j;
- unsigned int somme = 0;
+unsigned int j;
+unsigned int somme = 0;
 
-if( n == 0)
-    return '\0';
-    
-va_start(ap , n);
+if (n == 0)
+return ('\0');
+
+va_start(ap, n);
 for (j = 0; j < n; j++)
 {
 somme += va_arg(ap, int);
