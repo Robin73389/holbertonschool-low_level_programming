3ee592d486ca1b3d2acf856d033def9e11aeb0a9
Robin73389 <12229@holbertonstudents.com>
2025-12-04 09:46:29 +0100

    Write a function that prints all the elements

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stddef.h>
+
+/**
+ * print_list - This is name for the prototype
+ * @h: this is the name for the structure
+ * Return: (count)
+*/
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		h = h->next;
+		count++;
+	}
+	return (count);
+
+}
