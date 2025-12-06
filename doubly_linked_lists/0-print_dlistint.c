3c32660f10c0251cdbc846bda25a2b1254d369d0
Robin73389 <12229@holbertonstudents.com>
2025-12-06 23:57:19 +0100

    I corrected the output; my 8 wasn't below 9.

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -15,8 +15,6 @@ size_t print_dlistint(const dlistint_t *h)
 	size_t count = 0;
 	const dlistint_t *temp = h;
 
-	printf("Liste de transfert\n");
-
 	while (temp != NULL)
 	{
 		printf("%d\n", temp->n);
