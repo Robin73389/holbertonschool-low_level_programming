31b5de93ebbf3802b3f054c6bda338f7d1746c62
Robin73389 <12229@holbertonstudents.com>
2025-12-10 00:18:15 +0100

    Correction the code

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -13,18 +13,21 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i;
 	va_list args;
+	unsigned int i;
+
 	va_start(args, n);
+
 	for (i = 0; i < n; i++)
 	{
-	   printf ("%d", va_arg(args, const unsigned int));
-		printf ("%s", separator);
+		unsigned int num = va_arg(args, unsigned int);
+
+		printf("%u", num);
+
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
 	}
+
 	printf("\n");
 	va_end(args);
-
-
-	return;
-
 }
