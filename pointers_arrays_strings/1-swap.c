9f29d17fa0fb0bda21667e8e9865c4f3e27e6d82
Robin73389 <12229@holbertonstudents.com>
2025-12-23 00:36:07 +0100

    Change the syntax and add the betty style

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,8 +1,17 @@
 #include"main.h"
 
+/**
+ * swap_int - This is the name for the function
+ * @a: the caractére
+ * @b: the second caractére
+ */
+
 void swap_int(int *a, int *b)
 {
-	int temp = *a;
-	*a = *b;
-	*b = temp;
+
+	int temp = a[0];
+
+	a[0] = b[0];
+	b[0] = temp;
+
 }
