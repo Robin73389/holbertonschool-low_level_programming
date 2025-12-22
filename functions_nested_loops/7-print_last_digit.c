01f4a0f1f4bd29453b9c7bc220c1275e83e77835
Robin73389 <12229@holbertonstudents.com>
2025-12-22 21:19:27 +0100

    Change the synstac

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,14 @@
 int print_last_digit(int n)
 {
 
-	n = n % 10;
+	int last;
 
-	if (n < 0)
-		n = -n;
+	last = n % 10;
 
-	_putchar(n + '0');
+	if (last < 0)
+		last = -last;
 
-	return (0);
+	_putchar(last + '0');
+
+	return (last);
 }
