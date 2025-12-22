8e0af7fe69d1a1544a3c948d2232d4eac13cea7a
Robin73389 <12229@holbertonstudents.com>
2025-12-22 20:22:21 +0100

    change the syntax and add the betty style

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,7 +1,17 @@
 #include "main.h"
 
+/**
+ * _abs - function which allow the absolute value
+ * @x: This is the number
+ * Return: (0)
+ */
+
 int _abs(int x)
-{	
-	int y = (x >> 31);
-	return (x ^ y) - y;
+{
+	if (x < 0)
+	{
+		x = (-1 * x);
+	}
+	putchar(x);
+	return (0);
 }
