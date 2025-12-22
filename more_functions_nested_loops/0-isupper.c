a0ea3d29006324d12d21eb5aea12c1b6be72df35
Robin73389 <12229@holbertonstudents.com>
2025-12-22 21:30:19 +0100

    Change the syntaxt

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,23 @@
 #include"main.h"
 #include<stdio.h>
+#include<ctype.h>
+
+/**
+ * _isupper - THis is the function which allows the check uppercase caractére
+ * @c: This is the caractére
+ * Return: (0)
+ */
 
 
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (isupper(c))
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
+
 }
