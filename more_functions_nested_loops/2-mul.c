d5265cc2814e19312760e42a31289a40bb237a3d
Robin73389 <12229@holbertonstudents.com>
2025-12-22 21:40:34 +0100

    Add the style betty

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,8 +1,15 @@
 #include "main.h"
+
+/**
+ * mul - This is the function for mult two inter
+ * @a: the first number
+ * @b: the second number
+ * Return: (n)
+ */
 int mul(int a, int b)
 {
-	
+
 	int n = a * b;
 
-	return n;
+	return (n);
 }
