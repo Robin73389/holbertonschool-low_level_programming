645a3f87b1c16cb890320489a8759e387ceda793
Robin73389 <12229@holbertonstudents.com>
2025-12-22 21:48:54 +0100

    Change the code

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,18 @@
 #include"main.h"
 
+/**
+ * print_numbers - This is the name for the function
+ */
+
 void print_numbers(void)
 {
-	int n = 48;
-	while(n <= 57)
-	{	
+	int n = '0';
+
+	while (n <= '9')
+	{
 		_putchar(n);
 		n++;
-		
 	}
 	_putchar('\n');
-}	
+
+}
