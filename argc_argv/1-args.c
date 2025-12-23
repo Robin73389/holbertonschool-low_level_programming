7e9d84986bab7a0f6ffcf64e9239d350d93a9df3
Robin73389 <12229@holbertonstudents.com>
2025-12-23 15:46:42 +0100

    Change the code and Add style betty

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main - This is function main
+ * @argc: This the argument
+ * @argv: This the arrays of argument
+ * Return: (0)
+ */
+
 int main(int argc, char *argv[])
 {
-    (void)argv;   /* on dit au compilateur "je n'utilise pas argv" */
-    printf("%d\n", argc - 1);
-    return (0);
+	argv[0] = NULL;
+
+	printf("%d\n", argc - 1);
+
+	return (0);
 }
