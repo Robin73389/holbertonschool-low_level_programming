078c28b8530675f4455bd90364dde08af7e04a12
Robin73389 <12229@holbertonstudents.com>
2025-12-23 12:19:09 +0100

    Add the betty style and change the code

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,16 +1,26 @@
 #include"main.h"
+#include<string.h>
 
+/**
+ * print_rev - This is the name for the function
+ * @s: This is the string
+ */
 
 void print_rev(char *s)
-{	
-	int count, i;
+{
+	int i;
+	int j;
 
-	for (count = 0; s[count] != '\0'; count++)
+	i = 0;
+	j = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	for (j = i - 1; j >= 0 ; j--)
 	{
+		_putchar(s[j]);
 	}
-	for (i = count -1 ; i >= 0; i--)
-	{	_putchar(s[i]);
-	}
+
 	_putchar('\n');
-                        
 }
