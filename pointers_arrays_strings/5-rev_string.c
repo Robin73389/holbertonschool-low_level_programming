284d296afa910c2f6c2a0de0e0e7c013a799c0fa
Robin73389 <12229@holbertonstudents.com>
2025-12-23 12:24:13 +0100

    Add the betty and change the code

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,22 +1,28 @@
 #include"main.h"
 #include<string.h>
-void rev_string(char *s)
-{	
-	char tmp;
-	int end = 0;
-	int start = 0;
-	while(s[end] != '\0')	
-		end++;
-	end--;
-	while ( start < end)
-	{
-		tmp = s[start];
-		s[start] = s[end];
-		s[end] = tmp;
 
-		start++;
-		end--;
+/**
+ * rev_string - This is the name for the function
+ * @s: This is the string
+ */
+void rev_string(char *s)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+		_putchar(s[i]);
 	}
 
+	for (j = i - 1; j >= 0 ; j--)
+	{
+		_putchar(s[j]);
+	}
 
-}		
+	_putchar('\n');
+}
