0e34927a72c7cd591762c236914fc8791c11c68e
Robin73389 <12229@holbertonstudents.com>
2025-12-23 18:30:18 +0100

    Add the memory

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -10,14 +10,6 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-
-	if (d == NULL)
-	{
-		return;
-	}
-
-	d = malloc(sizeof(struct dog));
-
 	d->age = age;
 	d->name = name;
 	d->owner = owner;
