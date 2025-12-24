7457f1c71e76de9edc3b6873d0940e98a269f860
Robin73389 <12229@holbertonstudents.com>
2025-12-24 18:13:43 +0100

    Change the code

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -3,31 +3,31 @@
 #include <stdio.h>
 
 /**
- * print_numbers - This is the name for the my function
- *
- * @separator: This is the comma
- * @n: This is the number
- * Return: (0);
+ *print_numbers - This is the name for the my function
+ *@separator: This is the comma
+ *@n: This is the number
+ *Return: (0);
  */
 
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list args;
+	va_list(args);
 	unsigned int i;
+
+	if (separator == NULL)
+	{
+		return;
+	}
 
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
-		unsigned int num = va_arg(args, unsigned int);
-
-		printf("%u", num);
-
-		if (separator != NULL && i < n - 1)
-			printf("%s", separator);
+		printf("%d%s", va_arg(args, int), separator);
 	}
 
 	printf("\n");
 	va_end(args);
+
 }
