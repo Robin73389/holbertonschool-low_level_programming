639605f1d2dc1e832aa0e56a55b566adb7beb09c
Robin73389 <12229@holbertonstudents.com>
2026-01-07 02:37:40 +0100

    Correction the allocation memory

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -14,15 +14,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog2;
 
-	dog2 = malloc(sizeof(dog_t));
-	if (dog2 == NULL)
+	dog2 = malloc(sizeof(dog2->name));
+	if (name == NULL)
 	{
 		free(dog2);
 		return (NULL);
 	}
-	dog2->name = name;
+	dog2 = malloc(sizeof(dog2->owner));
+	if (owner == NULL)
+	{
+		return (NULL);
+	}
 	dog2->age = age;
-	dog2->owner = owner;
 
 	return (dog2);
 }
