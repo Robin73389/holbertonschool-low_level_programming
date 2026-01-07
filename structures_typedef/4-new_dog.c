1eb5405dfe81f63a4c034a8690bc956eed88225f
Robin73389 <12229@holbertonstudents.com>
2026-01-07 02:44:03 +0100

    Create seconde dogs

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -14,17 +14,15 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog2;
 
-	dog2 = malloc(sizeof(dog2->name + 1));
-	if (name == NULL)
+	dog2 = malloc(sizeof(dog_t));
+	if (dog2 == NULL)
 	{
+		free(dog2);
 		return (NULL);
 	}
-	dog2 = malloc(sizeof(dog2->owner));
-	if (owner == NULL)
-	{
-		return (NULL);
-	}
+	dog2->name = name;
 	dog2->age = age;
+	dog2->owner = owner;
 
 	return (dog2);
 }
